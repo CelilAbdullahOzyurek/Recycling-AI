26ffe4492c02a32763d66ff9337ae23b6f819b18
CelilAbdullahOzyurek <celilbus@outlook.com>
2025-04-29 17:36:31 +0300

    RecyclingModelAI

---
+++ cam.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+  
+
+# Modeli yükle
+model = load_model('modelFinal_41.h5')
+capture= cv.VideoCapture(0)
+while True:
+    # Görüntüyü oku
+    ret, frame = capture.read()
+    frame=cv.resize(frame, (256, 256))
+    test_img = img_to_array(frame)
+    test_img = test_img / 255.0
+    test_img = np.expand_dims(test_img, axis=0)
+    result= model.predict(test_img)
+    result[0][0]
+    if result[0][0] > 0.5:
+        label = f"Paper ({result[0][0]*100:.2f}%)"
+    else:
+        label = f"Plastic ({(1-result[0][0])*100:.2f}%)"
+    font = cv.FONT_HERSHEY_SIMPLEX
+    cv.putText(frame, label, (10, 40), font, 1, (255, 0, 0), 3)
+    cv.imshow('predict', frame)
+    if cv.waitKey(1) & 0xFF == ord('d'):
+        break
+cv.destroyAllWindows()
+capture.release()
+print("closed")
---
+++ model.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+import os
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("TensorFlow version:", tf._version_)
+
+# To multiply images in the dataset
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    validation_split=0.2  # %20 seperated for validation
+)
+
+# Train data
+train_generator = train_datagen.flow_from_directory(
+    'waste/',
+    target_size=(256, 256),
+    batch_size=16,
+    class_mode='binary',
+    subset='training'
+)
+
+# Validation data
+validation_generator = train_datagen.flow_from_directory(
+    'waste/',
+    target_size=(256, 256),
+    batch_size=16,
+    class_mode='binary',
+    subset='validation'
+)
+
+# Model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Conv2D(256, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Flatten(),
+    Dropout(0.5), # Disconnect some neurons to prevent overfitting
+    Dense(512, activation='relu'),
+    Dense(1, activation='sigmoid') # For binary classification
+])
+
+# Compiling the model
+model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+
+# Training the model
+history = model.fit(
+    train_generator,
+    validation_data=validation_generator,
+    epochs=40
+)
+
+print("Model Training comleted")
+# Saving the model
+model.save('modelFinal_41.h5')
+print("Model Saved")
